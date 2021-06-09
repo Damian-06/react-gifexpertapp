48f6dc26ff7692bcd803ffe3c0228eb51e5ed5bc
Damian Garcia <157o02774@itsx.edu.mx>
2021-06-09 13:19:34 -0500

    First commit

---
+++ src/components/AddCategory.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types';
+
+
+export const AddCategory = ({ setCategories }) => {
+    
+    const [inputValue, setInputValue] = useState('');
+
+    const handleInputChange = ( e ) => {
+        setInputValue(e.target.value);
+    }
+
+    const handleSbmit = (e) => {
+        e.preventDefault();
+
+        if ( inputValue.trim().length > 2 ){
+        setCategories( cats => [ inputValue, ...cats, ] );
+        setInputValue('');
+        }
+
+    }
+
+    return (
+        <form onSubmit={ handleSbmit }>
+            <input
+                type="text"
+                value = {inputValue}
+                onChange = { handleInputChange }
+            />
+        </form>
+    )
+}
+
+AddCategory.protoTypes = {
+     setCategories: PropTypes.func.isRequired
+}
